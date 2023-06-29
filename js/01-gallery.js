3c94065d215b4e3d372639b72a52218c48f9ccd0
Liudmila-Ridko <125599487+liussi@users.noreply.github.com>
2023-06-29 21:00:25 +0300

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({preview, original, description}) => `<li class="gallery__item">
+<a class="gallery__link" onclick="return false" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>
+
+`)
+galleryEl.insertAdjacentHTML('beforeend', markup.join(''));
+galleryEl.addEventListener('click', onClick);
+
+function onClick (event) {
+    const isCardEl = event.target.classList.contains('.gallery__link')
+    
+if(isCardEl){
+    return;
+}
+const galleryImage = event.target.dataset.source;
+galleryEl.src = galleryImage;
+
+const instance = basicLightbox.create(`
+    <img src="${galleryImage}">
+`)
+
+instance.show()
+
+}
+
+
+
